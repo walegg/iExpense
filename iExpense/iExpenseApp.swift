2ef158d3a3b1e29bc1620daa9746c8174a4bd3a1
Louis Mille <mille.louis1@gmail.com>
2023-07-17 14:03:24 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Louis Mille on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
